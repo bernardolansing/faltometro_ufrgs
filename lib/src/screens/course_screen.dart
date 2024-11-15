8f441c844cb30e31cb860f4a7f9c3926ec93b6cd
Bernardo Lansing <bernardolans@gmail.com>
2024-11-14 23:48:04 -0300

    closing the keyboard when tapping out of text field

--- lib/src/screens/course_screen.dart
+++ lib/src/screens/course_screen.dart
@@ -74,92 +74,102 @@ class _CourseScreenState extends State<CourseScreen> {
     Navigator.of(context).pop(true);
   }
 
-  // TODO: use opacity to give more focus on the text input.
+  void _closeKeyboard() => FocusManager.instance.primaryFocus?.unfocus();
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(leading: _appBarIcon, title: _appBarTitle),
-    resizeToAvoidBottomInset: false,
-    body: SafeArea(
-      maintainBottomViewPadding: true, // Prevents a flickering when the
-      // keyboard is closed.
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _titleController,
-              // setState is needed to update the _buttonAvailable
-              // condition:
-              onChanged: (value) => setState(() {}),
-              autofocus: _isCreatingCourse, // Autofocus only
-              // when the course is being created, not when is edited.
-              textCapitalization: TextCapitalization.sentences, // Makes
-              // the text start with upper case.
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).colorScheme.onSurface,
-              ),
-              decoration: const InputDecoration(
-                labelText: 'Nome da disciplina',
-                labelStyle: TextStyle(color: Colors.grey),
-              ),
-            ),
-
-            const Spacer(flex: 3),
-
-            const Text(
-              'Marque a quantidade de períodos de aula por dia de semana.',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 4),
-            const Text(
-              _periodsExplanationText,
-              textAlign: TextAlign.center,
-            ),
-
-            const Spacer(),
-
-            ...Iterable.generate(6, (index) => ListTile(
-              title: Text(weekdaysNames[index]),
-              visualDensity: VisualDensity.compact,
-              contentPadding: const EdgeInsets.symmetric(horizontal: 4),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    onPressed: () => _decreasePeriods(index),
-                    icon: PhosphorIcon(PhosphorIcons.bold.minus),
-                    splashRadius: 28,
-                  ),
-                  Text(_periodsPerWeekday[index].toString()),
-                  IconButton(
-                    onPressed: () => _increasePeriods(index),
-                    icon: PhosphorIcon(PhosphorIcons.bold.plus),
-                    splashRadius: 28,
-                  ),
-                ],
-              ),
-            )),
-
-            const Spacer(flex: 3),
-
-            SizedBox(
-              width: 200,
-              child: ElevatedButton(
-                onPressed: _buttonAvailable ? _buttonAction : null,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    PhosphorIcon(PhosphorIcons.regular.floppyDisk),
-                    const Text('Salvar'),
-                    const Icon(Icons.save, color: Colors.transparent),
-                  ],
+  Widget build(BuildContext context) => GestureDetector(
+    onTap: _closeKeyboard,
+    onVerticalDragStart: (details) => _closeKeyboard(),
+    onHorizontalDragStart: (details) => _closeKeyboard(),
+    child: Scaffold(
+      appBar: AppBar(leading: _appBarIcon, title: _appBarTitle),
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        maintainBottomViewPadding: true, // Prevents a flickering when the
+        // keyboard is closed.
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _titleController,
+                onChanged: (value) => setState(() {}), // setState is needed to
+                // update the _buttonAvailable condition.
+                autofocus: _isCreatingCourse,
+                textCapitalization: TextCapitalization.sentences, // Makes
+                // the text start with upper case.
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+                decoration: const InputDecoration(
+                  labelText: 'Nome da disciplina',
+                  labelStyle: TextStyle(color: Colors.grey),
                 ),
               ),
-            ),
-          ],
+
+              const Spacer(flex: 3),
+
+              const Text(
+                'Marque a quantidade de períodos de aula por dia de semana.',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 4),
+              const Text(
+                _periodsExplanationText,
+                textAlign: TextAlign.center,
+              ),
+
+              const Spacer(),
+
+              ...Iterable.generate(6, (index) => ListTile(
+                title: Text(weekdaysNames[index]),
+                visualDensity: VisualDensity.compact,
+                contentPadding: const EdgeInsets.symmetric(horizontal: 4),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        _closeKeyboard();
+                        _decreasePeriods(index);
+                      },
+                      icon: PhosphorIcon(PhosphorIcons.bold.minus),
+                      splashRadius: 28,
+                    ),
+                    Text(_periodsPerWeekday[index].toString()),
+                    IconButton(
+                      onPressed: () {
+                        _closeKeyboard();
+                        _increasePeriods(index);
+                      },
+                      icon: PhosphorIcon(PhosphorIcons.bold.plus),
+                      splashRadius: 28,
+                    ),
+                  ],
+                ),
+              )),
+
+              const Spacer(flex: 3),
+
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: _buttonAvailable ? _buttonAction : null,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      PhosphorIcon(PhosphorIcons.regular.floppyDisk),
+                      const Text('Salvar'),
+                      const Icon(Icons.save, color: Colors.transparent),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     ),
