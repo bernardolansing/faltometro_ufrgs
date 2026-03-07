ee9065f57dd76ade1649fc7f8bcd6ebe9af39671
Bernardo Lansing <bernardolans@gmail.com>
2026-03-07 16:45:19 -0300

    download routes send back the new generation number; also fixed encoding issues with their responses

--- Backend/src/Controllers/DownloadCourseOptionsController.cs
+++ Backend/src/Controllers/DownloadCourseOptionsController.cs
@@ -1,4 +1,4 @@
-using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using FaltometroUfrgsBackend.Models;
 using FaltometroUfrgsBackend.Services;
@@ -14,33 +14,38 @@ public class DownloadCourseOptionsController(AppDatabase db) : Controller
     /// <summary>
     /// Streams (if necessary) the list of course options from the database.
     ///
-    /// First of all, if the provided generation number is updated, the response is a no-body 200. Otherwise, the
-    /// response is a stream of JSON lines (JSONL, in other words each line is a serialized JSON object), each one
-    /// corresponding to a course option. This allows the consumer to process data on-the-fly.
+    /// First of all, if the provided generation number is updated, the response is a no-body 200. If the generation
+    /// number is not provided or is outdated, the response is going to be a stream of lines in which:
+    /// -> The first line ever is the updated generation number as a string.
+    /// -> Every subsequent line is a valid JSON object representing a course option object (JSONL format).
+    /// This design allows the consumer to process data on-the-fly.
     /// </summary>
     /// <param name="generation">The courses list generation number that the consumer currently has downloaded. If
     /// updated, there'll be no need for a response.</param>
     [HttpGet]
     public async Task Get(uint? generation)
     {
-        // User has submitted a generation number, we must check if it matches with the most recent generation.
-        if (generation != null)
-        {
-            var result = await db.Generations.Where(g => g.Id == Generation.CourseOptionsGenerationId)
-                .Select(g => g.GenerationNumber)
-                .ToListAsync();
-            // User already has the most recent courses downloaded:
-            if (generation == result.First())
-                return;
-        }
+        var updatedGenerationNumber = await db.Generations.Where(g => g.Id == Generation.CourseOptionsGenerationId)
+            .Select(g => g.GenerationNumber)
+            .FirstAsync();
 
+        // Check if the user has the updated generation downloaded. If so, we can just finish this request.
+        if (generation == updatedGenerationNumber)
+            return;
+        
+        // Otherwise, we'll stream the course options. But first, let's configure the headers and send the updated
+        // generation number.
+        Response.Headers.Append("Content-Type", "text/plain; charset=utf-8");
+        await Response.WriteAsync($"{updatedGenerationNumber}\n");
+
+        // This option is required so JsonSerializer won't escape non-ASCII characters to \u sequences.
+        var serializerOptions = new JsonSerializerOptions { Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping };
         var enumerable = db.CourseOptions.Include(p => p.CourseOptionsClassSessions)
             .AsAsyncEnumerable();
         await foreach (var course in enumerable)
         {
-            var line = JsonSerializer.Serialize(course) + '\n';
-            var mem = new ReadOnlyMemory<byte>(Encoding.UTF8.GetBytes(line));
-            await Response.BodyWriter.WriteAsync(mem);
+            var line = JsonSerializer.Serialize(course, serializerOptions) + '\n';
+            await Response.WriteAsync(line);
         }
     }
 }
