90e2db8ea8acd3285152894ac7d0229e49c4c0cb
Bernardo Lansing <bernardolans@gmail.com>
2026-03-07 21:15:31 -0300

    new storage manager is based in interfaces and generic types

--- app/lib/src/storage_manager.dart
+++ app/lib/src/storage_manager.dart
@@ -6,32 +6,51 @@ import 'package:path_provider/path_provider.dart';
 
 class StorageManager {
   static late final Directory _basePath;
-  
+
   static Future<void> initialize() async {
     _basePath = await getApplicationDocumentsDirectory();
   }
-  
-  static StorageEntry getEntry(String name) =>
-      _FileStorageEntry(File('${_basePath.path}/$name.json'));
+
+  static StorageEntry<T> getEntry<T>(String name, JsonConverter<T> converter) {
+    return _FileStorageEntry<T>(
+      file: File('${_basePath.path}/$name.json'),
+      jsonConverter: converter,
+    );
+  }
+
+}
+
+/// Object capable of expressing `T` as a JSON-like object, or parsing such
+/// object to retrieve an instance of `T`.
+abstract class JsonConverter<T> {
+  T fromJson(dynamic json);
+
+  dynamic toJson(T self);
 }
 
 /// Represents a JSON content that is saved locally, requiring no network
 /// transactions to be accessed.
-abstract class StorageEntry {
+abstract class StorageEntry<T> {
   /// Reads the JSON content assigned to this entry and returns it deserialized.
   /// If no content could be found, throws [MissingStorageEntry].
-  Future<dynamic> load();
+  Future<T> load();
 }
 
 /// JSON storage based on files. Currently the only available implementation for
 /// [StorageEntry].
-class _FileStorageEntry implements StorageEntry {
+class _FileStorageEntry<T> implements StorageEntry<T> {
   final File _file;
-  
-  _FileStorageEntry(this._file);
+  final JsonConverter<T> _converter;
+
+  _FileStorageEntry({
+    required File file,
+    required JsonConverter<T> jsonConverter,
+  }) :
+        _file = file,
+        _converter = jsonConverter;
 
   @override
-  Future<dynamic> load() async {
+  Future<T> load() async {
     final fileName = _file.uri.pathSegments.last;
     final fileExists = await _file.exists();
 
@@ -47,16 +66,18 @@ class _FileStorageEntry implements StorageEntry {
       throw MissingStorageEntry();
     }
 
+    late final dynamic jsonDeserialized;
     try {
-      return jsonDecode(content);
-    }
-    catch (error, stackTrace) {
+      jsonDeserialized = jsonDecode(content);
+    } catch (error, stackTrace) {
       log(
         '[STORAGE] error while decoding file $fileName: $error',
         stackTrace: stackTrace,
       );
       throw MissingStorageEntry();
     }
+
+    return _converter.fromJson(jsonDeserialized);
   }
 }
 
